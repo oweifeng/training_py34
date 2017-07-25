40c84b76bf03422c421bdd95255f97d0d5cea276
oweifeng <oweifeng@qq.com>
2017-07-26 06:54:50 +0800

    commit again

--- apps/message/views.py
+++ apps/message/views.py
@@ -5,19 +5,23 @@ from .models import UserMessage
 
 
 def getform(request):  # Http request object
-    # all_messages = UserMessage.objects.filter(name='bobby')
-    # for message in all_messages:
-    #     print(message.name)
-    if request.method == "POST":
-        name = request.POST.get('name', '')
-        message = request.POST.get('message', '')
-        address = request.POST.get('address', '')
-        email = request.POST.get('email', '')
-        user_message = UserMessage()
-        user_message.name = name
-        user_message.message = message
-        user_message.address = address
-        user_message.email = email
-        user_message.object_id = 'helloworld3'
-        user_message.save()
-    return render(request, 'message/form.html')
+    message = None
+    all_messages = UserMessage.objects.filter(name='bobby')
+    for message in all_messages:
+        message = all_messages[0]
+
+    # if request.method == "POST":
+    #     name = request.POST.get('name', '')
+    #     message = request.POST.get('message', '')
+    #     address = request.POST.get('address', '')
+    #     email = request.POST.get('email', '')
+    #     user_message = UserMessage()
+    #     user_message.name = name
+    #     user_message.message = message
+    #     user_message.address = address
+    #     user_message.email = email
+    #     user_message.object_id = 'helloworld3'
+    #     user_message.save()
+    return render(request, 'message/form.html',{
+        "my_message": message
+    })
